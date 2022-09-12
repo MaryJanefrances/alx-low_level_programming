6e00b1662bc29e0073b8aaf3954d820dbec5e11c
MaryJanefrances <nledomary1203@gmail.com>
2022-09-12 11:53:44 +0000

    size

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+
+**
+ * main program entry point.
+ *
+ * Return: 1 on successs.
+ * /in main(void)
+ * {
+ * 	write(2, "and that piece of art is still usefull" - Dora korper, 2015-19-19
+ * 	return: (1);
+ * }
