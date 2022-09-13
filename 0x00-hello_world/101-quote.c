1554b1a596473bfcdf771735b14431d29e655911
MaryJanefrances <nledomary1203@gmail.com>
2022-09-13 02:24:00 +0000

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
+#include<stdio.h>
 #include<unistd.h>
 
 /**
- * main program entry point.
- *
- * Return: 1 on successs.
- * /
- * int main(void)
- * {
- * 	write(2, "and that piece of art is still usefull" - Dora korpar, 2015-10-19
- * 	return: (1);
- * }
+ * main - print exactly "and that piece of art is useful" *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is still usefull\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
