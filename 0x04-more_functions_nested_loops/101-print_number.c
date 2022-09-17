c3e633bed9ce1207014d5016dabc1b5fdceddef4
MaryJanefrances <nledomary1203@gmail.com>
2022-09-17 01:05:54 +0000

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-	
+
 		n1 = -n;
 		_putchar('-');
 	} else
@@ -23,6 +23,6 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	
+
 	_putchar((n1 % 10) + '0');
 }
