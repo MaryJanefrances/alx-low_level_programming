4516803405560a4c5c397a6ac4e69e9f1128f0d8
MaryJanefrances <nledomary1203@gmail.com>
2022-09-26 23:01:42 +0000

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,14 +7,14 @@
  * @a: square matrix of which we print the sum of diagonals
  * @size: size of the matrix
  */
-void print_diagsums(int*a, int size)
+void print_diagsums(int *a, int size)
 {
 	int i;
 
 	unsigned int sum, sum1;
 	
-	sum = 0
-	sum1 = 0
+	sum = 0;
+	sum1 = 0;
 	
 	for (i = 0; i < size; i++)
 	{
